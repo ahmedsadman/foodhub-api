dba9d0db2b1c9b3da66ad94c062e3424e97291eb
Ahmed Sadman <ahmedsadman.211@gmail.com>
2019-02-19 17:38:23 +0600

    connect with mongodb atlas cluster

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +10,6 @@ const authRoutes = require('./routes/auth');
 /* ----------------- Middlewares --------------- */
 app.use(bodyParser.json());
 
-
 /* ----------------- Enable CORS ---------------- */
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,16 +17,23 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Methods',
         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
     );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
     next();
 });
-
 
 /* ---------------- ROUTES -------------------- */
 app.use('/auth', authRoutes);
 
-
 /* --------------- Server --------------------- */
-app.listen(port, () => {
-    console.log(`Server started at port ${port}`);
-});
+mongoose
+    .connect(
+        'mongodb+srv://admin:admin@samyo001-ksrpc.mongodb.net/foodhub?retryWrites=true',
+        { useNewUrlParser: true }
+    )
+    .then(() => {
+        app.listen(port, () => console.log(`Server started at port ${port}`));
+    })
+    .catch(err => console.log(`ERROR: ${err}`));
