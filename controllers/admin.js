93ad766cf24e5338fe9501bc5756500dd66d3892
Ahmed Sadman Muhib <ahmedsadman.211@gmail.com>
2019-05-04 15:27:29 +0600

    user get/update routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,11 @@
 const Restaurant = require('../models/restaurant');
 const Review = require('../models/review');
+const User = require('../models/user');
 
 exports.createRestaurant = async (req, res, next) => {
     try {
         const restaurant = new Restaurant(req.body);
-        const response =  await restaurant.save();
+        const response = await restaurant.save();
 
         res.status(201).send({
             message: 'Successfully Created',
@@ -23,7 +24,7 @@ exports.deleteRestaurant = async (req, res, next) => {
     try {
         const { id } = req.query;
         const response = await Restaurant.findByIdAndDelete(id);
-        
+
         if (response) {
             res.send({
                 message: 'Successfully Deleted'
@@ -31,9 +32,8 @@ exports.deleteRestaurant = async (req, res, next) => {
         } else {
             res.status(400).send({
                 message: 'Not Found'
-            })
+            });
         }
-        
     } catch (e) {
         res.status(400).send({
             message: 'Error occured',
@@ -46,11 +46,15 @@ exports.updateRestaurant = async (req, res, next) => {
     const { id } = req.params;
 
     try {
-        const response = await Restaurant.findByIdAndUpdate(id, {
-            ...req.body
-        }, {
-            new: true
-        });
+        const response = await Restaurant.findByIdAndUpdate(
+            id,
+            {
+                ...req.body
+            },
+            {
+                new: true
+            }
+        );
 
         if (response) {
             res.send({
@@ -68,4 +72,37 @@ exports.updateRestaurant = async (req, res, next) => {
             error: e
         });
     }
-}
+};
+
+exports.getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({}, 'username email privileged').sort({
+            username: 1
+        });
+        res.send({
+            data: users
+        });
+    } catch (e) {
+        res.status(400).send({
+            error: e.message
+        });
+    }
+};
+
+exports.updateUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const response = await User.findByIdAndUpdate(
+            id,
+            { ...req.body },
+            { new: true }
+        );
+        res.send({
+            data: response
+        })
+    } catch (e) {
+        res.status(400).send({
+            error: e.message
+        });
+    }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -6,5 +6,7 @@ const adminController = require('../controllers/admin');
 router.post('/restaurants/create', adminController.createRestaurant);
 router.delete('/restaurants/delete', adminController.deleteRestaurant);
 router.patch('/restaurants/update/:id', adminController.updateRestaurant);
+router.patch('/users/update/:id', adminController.updateUser)
+router.get('/users', adminController.getUsers);
 
 module.exports = router;
